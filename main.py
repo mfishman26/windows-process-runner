acf61c11acb791c555308ccd6fb4d657fa72a24c
Johnny Hairon <johnny.hairon@gmail.com>
2025-02-09 16:00:14 -0500

    basic setup stuff

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
-from time import sleep
+import psutil
+from psutil import AccessDenied
+
+
+KEY_DIR = "process-info"
+KEY_MODULE_NAME = "main.py"
+IS_PYTHON = True
+
+
+def main(root_direc: str, module_name: str):
+    for proc in psutil.process_iter():
+        try:
+            if IS_PYTHON:
+                if "python" in proc.name():
+                    print(proc)
+                    cmd_res = proc.cmdline()
+                    if root_direc in cmd_res[0] and module_name in cmd_res[1]:
+                        print("--FOUND--")
+                        print(cmd_res)
+            else:
+                if proc.name() == module_name and root_direc in proc.cmdline()[0]:
+                    # print(proc)
+                    print("--FOUND--")
+                    print(proc.cmdline())
+        except AccessDenied as e:
+            print(e)
+            print("Access denied, skipping...")
 
 
 if __name__ == "__main__":
-    while True:
-        print("test process blah blah")
-        sleep(5)
+    main(root_direc=KEY_DIR, module_name=KEY_MODULE_NAME)
